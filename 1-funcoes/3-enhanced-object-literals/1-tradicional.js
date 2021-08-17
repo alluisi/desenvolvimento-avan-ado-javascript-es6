0ad62d87009c7014edaa9fd0c291885f85dbc44f
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-08-17 17:02:43 -0300

    Enhanced Object Literals

---
+++ 1-funcoes/3-enhanced-object-literals/1-tradicional.js
@@ -0,0 +1,13 @@
+const prop2 = 'ES6';
+const obj1 = {
+    prop1: 'Digital Innovation One',
+    prop2: prop2
+};
+console.log(obj1);
+// { prop1: 'Digital Innovation One', prop2: 'ES6' }
+
+const propName = 'Alessandra';
+const obj2 = {};
+obj2[propName + 'Lamas'] = 'Luisi';
+console.log(obj2);
+// { AlessandraLamas: 'Luisi' }
