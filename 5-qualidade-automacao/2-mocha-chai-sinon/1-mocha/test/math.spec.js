bf2036f3f186c40fb402815c625643861246d876
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-08-19 18:58:45 -0300

    Mocha - Parte 2

--- 5-qualidade-automacao/2-mocha-chai-sinon/1-mocha/test/math.spec.js
+++ 5-qualidade-automacao/2-mocha-chai-sinon/1-mocha/test/math.spec.js
@@ -2,11 +2,17 @@ const assert = require('assert');
 const Math = require('../src/math');
 
 describe('Math class', function () {
-    it('Sum two numbers', function () {
+    it('Sum two numbers', function (done) { // não utilizar arrow function para poder chamar o this
         const math = new Math();
+        // this.timeout(3000); // para subir o limite de tempo do Mocha
 
-        assert.equal(math.sum(5, 5), 10);
+        math.sum(5, 5, (value) => {
+            assert.equal(value, 10);
+            done();
+        });
     });
+
+    it('Multiply tho numbers');
 });
 
 // npm run test
