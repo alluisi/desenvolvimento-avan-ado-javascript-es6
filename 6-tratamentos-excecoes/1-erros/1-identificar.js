ad04973daf7b8667a703b4052b4b2bcbca469bf0
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-08-20 11:52:59 -0300

    Tratamento de erros

---
+++ 6-tratamentos-excecoes/1-erros/1-identificar.js
@@ -0,0 +1,6 @@
+console.log(name);
+const name = 'Alessandra';
+// ReferenceError: Cannot access 'name' before initialization
+
+console.log('Keep going...');
+// A sequência do código foi interrompida devido ao erro
